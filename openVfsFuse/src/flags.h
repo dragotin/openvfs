064acec8d39eba029d45af57368994321a2cc488
Hannah von Reth <h.vonreth@opencloud.eu>
2025-12-10 13:16:43 +0100

    Apply clang format

--- openVfsFuse/src/flags.h
+++ openVfsFuse/src/flags.h
@@ -3,26 +3,25 @@
 
 #include <map>
 
-template<typename T>
+template <typename T>
 class OFlags
 {
-    public:
+public:
     OFlags(std::string name)
-        :name(name)
-    {
+        : name(name) {
 
-    };
+        };
     std::string name;
     std::map<T, std::string> names;
-
 };
 
-template<typename T>
+template <typename T>
 class OFlag
 {
 public:
     OFlag(const OFlags<T> &type, uint64_t v)
-        :type(type), val(v)
+        : type(type)
+        , val(v)
     {
     }
 
@@ -31,22 +30,18 @@ public:
 };
 #define ADD_O_FLAG(CLASS, KEY) CLASS.names[KEY] = #KEY;
 
-template<typename T>
+template <typename T>
 std::ostream &operator<<(std::ostream &out, OFlag<T> o)
 {
     out << o.type.name << "(";
     constexpr auto size = sizeof(o.val) * 8;
-    for (uint64_t p=0;p<size; ++p)
-    {
+    for (uint64_t p = 0; p < size; ++p) {
         const T key = (0x1 << p);
-        if (o.val & key)
-        {
+        if (o.val & key) {
             const auto it = o.type.names.find(key);
-            if (it != o.type.names.end())
-            {
+            if (it != o.type.names.end()) {
                 out << it->second;
-            } else
-            {
+            } else {
                 out << std::bitset<size>(key) << " 0x" << std::hex << key << std::dec;
             }
             out << "|";
--- openVfsFuse/src/sharedmap.cpp
+++ openVfsFuse/src/sharedmap.cpp
@@ -1,35 +1,32 @@
 /*
-* openvfsfuse - a Fuse layer to handle virtual filesystem items of cloud storage
-* Copyright (C) 2025  Klaas Freitag <k.freitag@opencloud.eu>
-*
-* This program is free software: you can redistribute it and/or modify
-* it under the terms of the GNU General Public License as published by
-* the Free Software Foundation, either version 3 of the License, or
-* (at your option) any later version.
-*
-* This program is distributed in the hope that it will be useful,
-* but WITHOUT ANY WARRANTY; without even the implied warranty of
-* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-* GNU General Public License for more details.
-*
-* You should have received a copy of the GNU General Public License
-* along with this program.  If not, see <http://www.gnu.org/licenses/>.
-*/
+ * openvfsfuse - a Fuse layer to handle virtual filesystem items of cloud storage
+ * Copyright (C) 2025  Klaas Freitag <k.freitag@opencloud.eu>
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
 
 #include "sharedmap.h"
 
-SharedMap::SharedMap()
-{
+SharedMap::SharedMap() { }
 
-}
-
-void SharedMap::insert(int key, const HydJob& value)
+void SharedMap::insert(int key, const HydJob &value)
 {
     std::lock_guard<std::mutex> lock(_mutex);
     _data[key] = value;
 }
 
-bool SharedMap::get(int key, HydJob& outValue)
+bool SharedMap::get(int key, HydJob &outValue)
 {
     std::lock_guard<std::mutex> lock(_mutex);
     auto it = _data.find(key);
@@ -40,7 +37,7 @@ bool SharedMap::get(int key, HydJob& outValue)
     return false;
 }
 
-bool SharedMap::set(int key, const HydJob& value)
+bool SharedMap::set(int key, const HydJob &value)
 {
     std::lock_guard<std::mutex> lock(_mutex);
 
@@ -78,8 +75,7 @@ long SharedMap::desktopClientPid()
 void SharedMap::printAll()
 {
     std::lock_guard<std::mutex> lock(_mutex);
-    for (auto& [k, v] : _data) {
+    for (auto &[k, v] : _data) {
         std::cout << k << ": " << v.state << "\n";
     }
 }
-
