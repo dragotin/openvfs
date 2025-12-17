2a7b0c1b6c0a59cb668e324f2c64b3f898efbf73
Hannah von Reth <h.vonreth@opencloud.eu>
2025-12-17 13:17:27 +0100

    Fix xattr log message

--- openVfsFuse/src/xattr.h
+++ openVfsFuse/src/xattr.h
@@ -2,13 +2,14 @@
 // SPDX-FileCopyrightText: 2025 Hannah von Reth <h.vonreth@opencloud.eu>
 
 #pragma once
+#include <filesystem>
 #include <optional>
 #include <string>
 
 namespace Xattr {
-int getxattr(const std::string &path, const char *name, char *value, size_t size);
+int getxattr(const std::filesystem::path &path, const char *name, char *value, size_t size);
 
 namespace CPP {
-    std::optional<std::string> getxattr(const std::string &path, const std::string &name);
+    std::optional<std::string> getxattr(const std::filesystem::path &path, const std::string &name);
 }
 }
