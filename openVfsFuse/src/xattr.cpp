c35fe8afedc739011cbba8a6800579e54651e8be
Hannah von Reth <h.vonreth@opencloud.eu>
2025-12-17 13:17:27 +0100

    Fix xattr log message

--- openVfsFuse/src/xattr.cpp
+++ openVfsFuse/src/xattr.cpp
@@ -3,26 +3,35 @@
 
 #include "xattr.h"
 
+#include <assert.h>
+#include <format>
+
 #include "openvfsfuse.h"
 
 #include <optional>
 #include <sys/xattr.h>
 
-int Xattr::getxattr(const std::string &path, const char *name, char *value, size_t size)
+int Xattr::getxattr(const std::filesystem::path &path, const char *name, char *value, size_t size)
 {
     ssize_t res = lgetxattr(path.c_str(), name, value, size);
-    if (res < 0 && errno == ENODATA) {
-        res = 0;
-    }
+
     // dont log "attrib not available" as error
-    openvfsfuse_log(path, "getxattr", res, "attrib name %s %s", name, errno == ENODATA ? "(attrib not found)" : value);
+    if (res < 0 && errno == ENODATA) {
+        openvfsfuse_log(path, "getxattr", 0, std::format("attrib {}: not found", name).c_str());
+
+    } else {
+        // use string view to ensure termination at size
+        openvfsfuse_log(path, "getxattr", res, std::format("attrib {}: {}", name, std::string_view(value, std::max<ssize_t>(0, res))).c_str());
+    }
+
     if (res < 0) {
         return -errno;
     }
+
     return static_cast<int>(res);
 }
 
-std::optional<std::string> Xattr::CPP::getxattr(const std::string &path, const std::string &name)
+std::optional<std::string> Xattr::CPP::getxattr(const std::filesystem::path &path, const std::string &name)
 {
     std::string value;
     int res = 0;
